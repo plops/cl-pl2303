9b9df75ea2b54858ebb8315a5c888809c1fad554
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-07-28 17:57:13 +0200

    delete some unnecessary comments

--- libusb0.lisp
+++ libusb0.lisp
@@ -18,23 +18,6 @@
 (defcfun "usb_find_busses" :int)
 (defcfun "usb_find_devices" :int)
 
-;; struct usb_device_descriptor {
-;;         u_int8_t  bLength;
-;;         u_int8_t  bDescriptorType;
-;;         u_int16_t bcdUSB;
-;;         u_int8_t  bDeviceClass;
-;;         u_int8_t  bDeviceSubClass;
-;;         u_int8_t  bDeviceProtocol;
-;;         u_int8_t  bMaxPacketSize0;
-;;         u_int16_t idVendor;
-;;         u_int16_t idProduct;
-;;         u_int16_t bcdDevice;
-;;         u_int8_t  iManufacturer;
-;;         u_int8_t  iProduct;
-;;         u_int8_t  iSerialNumber;
-;;         u_int8_t  bNumConfigurations;
-;; };
-
 (defctype u_int8_t :uint8)
 (defctype u_int16_t :uint16)
 (defctype u_int32_t :uint32)
@@ -54,18 +37,6 @@
   (iSerialNumber u_int8_t)  
   (bNumConfigurations u_int8_t))
 
-;; struct usb_device {
-;;   struct usb_device *next, *prev;
-;;   char filename[PATH_MAX + 1];
-;;   struct usb_bus *bus;
-;;   struct usb_device_descriptor descriptor;
-;;   struct usb_config_descriptor *config;
-;;   void *dev;            /* Darwin support */
-;;   u_int8_t devnum;
-;;   unsigned char num_children;
-;;   struct usb_device **children;
-;; };
-
 ;;find /usr/include/|grep -ir limits|xargs fgrep PATH_MAX
 #.(defconstant +path-max+ 4096)
 
@@ -81,14 +52,6 @@
   (num-children :unsigned-char)
   (children (:pointer ;usb_device
 	     )))
-
-;; struct usb_bus {
-;;   struct usb_bus *next, *prev;
-;;   char dirname[PATH_MAX + 1];
-;;   struct usb_device *devices;
-;;   u_int32_t location;
-;;   struct usb_device *root_dev;
-;; };
 
 (defcstruct usb_bus
   (next (:pointer ;:usb_bus
